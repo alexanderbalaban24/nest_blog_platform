54225f43577f8a5f153828ae27fdb035dda60ebf
alexanderbalaban24 <alexanderbalaban24@gmail.com>
2023-06-16 01:04:05 +0300

    refactoring

--- src/shared/helpers.ts
+++ src/shared/helpers.ts
@@ -12,18 +12,18 @@ export const queryHelper = {
 
     const skip = pageSize * (pageNumber - 1);
 
-    if (queryData.searchLoginTerm && queryData.searchNameTerm) {
+    if (queryData.searchLoginTerm && queryData.searchEmailTerm) {
       this.or([
         { login: new RegExp(queryData.searchLoginTerm, 'i') },
         { email: new RegExp(queryData.searchEmailTerm, 'i') },
       ]);
     } else {
-      /*if (queryData.searchLoginTerm) {
+      if (queryData.searchLoginTerm) {
         this.regex('login', new RegExp(queryData.searchLoginTerm, 'i'));
       }
       if (queryData.searchEmailTerm) {
         this.regex('email', new RegExp(queryData.searchEmailTerm, 'i'));
-      }*/
+      }
     }
 
     if (queryData.searchNameTerm) {
