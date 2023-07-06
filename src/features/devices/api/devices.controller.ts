7960f33d10782450955fa16d6e2f81e1cc44d130
alexanderbalaban24 <alexanderbalaban24@gmail.com>
2023-07-06 22:40:59 +0300

    fix security endpoint

--- src/features/devices/api/devices.controller.ts
+++ src/features/devices/api/devices.controller.ts
@@ -64,7 +64,7 @@ export class DevicesController extends ExceptionAndResponseHelper {
     const deviceResult = await this.DevicesQueryRepository.findDeviceById(
       deviceId,
     );
-    if ('currentUserId' !== deviceResult.payload.userId) {
+    if (currentUserId !== deviceResult.payload.userId) {
       throw new ForbiddenException();
     }
 
