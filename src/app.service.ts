523c2bd3af91ce8136b2773dd76a772353950980
alexanderbalaban24 <alexanderbalaban24@gmail.com>
2023-08-13 20:06:51 +0300

    add testing delete all data

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,13 +3,15 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Blog, BlogModelType } from './features/blogs/domain/blogs.entity';
 import { Post, PostModelType } from './features/posts/domain/posts.entity';
 import { User, UserModelType } from './features/users/domain/users.entity';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class AppService {
   constructor(
     @InjectModel(Blog.name) private BlogModel: BlogModelType,
     @InjectModel(Post.name) private PostModel: PostModelType,
-    @InjectModel(User.name) private UserModel: UserModelType,
+    @InjectDataSource() private dataSource: DataSource,
   ) {}
 
   async deleteAllData(): Promise<boolean> {
@@ -17,8 +19,12 @@ export class AppService {
       const deletedResult = await Promise.all([
         this.BlogModel.deleteMany({}),
         this.PostModel.deleteMany({}),
-        this.UserModel.deleteMany({}),
       ]);
+
+      await this.dataSource.query(`DELETE FROM "users_ban"`);
+      await this.dataSource.query(`DELETE FROM "users_email_confirmation"`);
+      await this.dataSource.query(`DELETE FROM "users_password_recovery"`);
+      await this.dataSource.query(`DELETE FROM "users_devices"`);
 
       return deletedResult.every((item) => item.deletedCount === 1);
     } catch (e) {
