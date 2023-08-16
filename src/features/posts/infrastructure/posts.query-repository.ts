2bee0790545f7acf3ffa41371ec45de1b0c26943
alexanderbalaban24 <alexanderbalaban24@gmail.com>
2023-08-16 21:45:44 +0300

    fix

--- src/features/posts/infrastructure/posts.query-repository.ts
+++ src/features/posts/infrastructure/posts.query-repository.ts
@@ -39,7 +39,7 @@ export class PostsQueryRepository {
     WHEN $1 = 'undefined' THEN p."blogId" ELSE $1::integer END
     ),
     "temp_data2" AS (
-    SELECT * FROM "temp_data1" as td
+    SELECT * FROM "temp_data1" AS td
     ORDER BY "${sortBy}" ${
         sortBy !== 'createdAt' ? 'COLLATE "C" ' : ''
       } ${sortDirection}
