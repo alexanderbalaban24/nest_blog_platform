280eec874e2c7296501defa209cc2cb28649e952
alexanderbalaban24 <alexanderbalaban24@gmail.com>
2023-08-19 22:05:19 +0300

    fix sql query

--- src/features/comments/infrastructure/comments.query-repository.ts
+++ src/features/comments/infrastructure/comments.query-repository.ts
@@ -5,7 +5,6 @@ import { InternalCode, LikeStatusEnum } from '../../../shared/enums';
 import { ViewCommentModel } from '../api/models/view/ViewCommentModel';
 import { QueryParamsCommentModel } from '../api/models/input/QueryParamsCommentModel';
 import { QueryBuildDTO, ResultDTO } from '../../../shared/dto';
-import { Types } from 'mongoose';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 
@@ -21,15 +20,89 @@ export class CommentsQueryRepository {
     query: QueryParamsCommentModel,
     userId?: string,
   ): Promise<ResultDTO<QueryBuildDTO<Comment, ViewCommentModel>>> {
-    const commentsData = await this.commentModel
-      .find({
-        postId,
-        isDeactivate: { $ne: false },
-      })
-      .findWithQuery<Comment, ViewCommentModel>(query);
-    commentsData.map((comment) => this._mapCommentToView(comment));
+    const sortBy = query.sortBy ?? 'createdAt';
+    const sortDirection = query.sortDirection ?? 'desc';
+    const pageNumber = query.pageNumber ? +query.pageNumber : 1;
+    const pageSize = query.pageSize ? +query.pageSize : 10;
+    const offset = pageSize * (pageNumber - 1);
 
-    return new ResultDTO(InternalCode.Success, commentsData);
+    const commentsRaw = await this.dataSource.query(
+      `
+     WITH "temp_data1" AS (SELECT pc."id", pc."content", u."id" AS "userId", u."login" AS "userLogin", pc."createdAt", p."id" AS "postId", p."title", p."blogId", b."name" AS "blogName",
+     (SELECT CAST(COUNT(*) AS INTEGER)
+     FROM "posts_comments_likes" AS pcl
+     LEFT JOIN "like_status_enum" AS lse
+     ON lse."id" = pcl."status"
+     WHERE lse."status" = '${LikeStatusEnum.Like}'
+     AND pcl."commentId" = pc."id"
+     ) AS "likesCount",
+     (SELECT CAST(COUNT(*) AS INTEGER)
+     FROM "posts_comments_likes" AS pcl
+     LEFT JOIN "like_status_enum" AS lse
+     ON lse."id" = pcl."status"
+     WHERE lse."status" = '${LikeStatusEnum.Dislike}'
+     AND pcl."commentId" = pc."id"
+     ) AS "dislikesCount",
+     (SELECT lse."status"
+     FROM "posts_comments_likes" AS pcl
+     LEFT JOIN "like_status_enum" AS lse
+     ON lse."id" = pcl."status"
+     WHERE lse."status" != '${LikeStatusEnum.None}' AND
+     pcl."userId" = $1 AND pcl."commentId" = pc."id"
+     ) AS "myStatus"
+    FROM "posts_comments" AS pc
+    LEFT JOIN "users" AS u
+    ON u."id" = pc."commentatorId"
+    LEFT JOIN "posts" AS p
+    ON p."id" = pc."postId"
+    LEFT JOIN "blogs_ban" AS bb
+    ON bb."blogId" = p."blogId"
+    LEFT JOIN "blogs" AS b
+    ON b."id" = p."blogId"
+    WHERE bb."isBanned" != true OR bb."isBanned" IS NULL
+    ),
+    "temp_data2" as (
+    SELECT * FROM "temp_data1" as td
+    ORDER BY "${sortBy}" ${
+        sortBy !== 'createdAt' ? 'COLLATE "C" ' : ''
+      } ${sortDirection}
+    LIMIT ${pageSize} OFFSET ${offset}
+    )
+    SELECT (
+    SELECT COUNT(*)
+    FROM "temp_data1"
+    ) as "totalCount",
+    (SELECT json_agg(
+    json_build_object(
+    'id', td."id", 'content', td."content", 'createdAt', td."createdAt", 'commentatorInfo',
+    json_build_object(
+    'userId', CAST(td."userId" AS TEXT), 'userLogin', td."userLogin"
+    ), 'likesInfo',
+    json_build_object(
+    'likesCount', td."likesCount",
+    'dislikesCount', td."dislikesCount",
+    'myStatus', td."myStatus"
+    )
+    )
+    ) FROM "temp_data2" AS td
+    ) AS "data"
+    `,
+      [userId],
+    );
+
+    const totalCount = +commentsRaw[0].totalCount;
+    const pagesCount = Math.ceil(totalCount / pageSize);
+    const data = new QueryBuildDTO<any, any>(
+      pagesCount,
+      pageNumber,
+      pageSize,
+      totalCount,
+      commentsRaw[0].data ?? [],
+    );
+
+    data.map(this._mapCommentToView);
+
+    return new ResultDTO(InternalCode.Success, data);
   }
 
   async findCommentById(
@@ -69,7 +142,7 @@ export class CommentsQueryRepository {
     LEFT JOIN "blogs_ban" AS bb
     ON bb."blogId" = p."blogId"
     WHERE pc."id" = $1 AND
-    bb."isBanned" != true
+    (bb."isBanned" != true OR bb."isBanned" IS NULL)
     `,
       [commentId, userId],
     );
@@ -100,19 +173,21 @@ export class CommentsQueryRepository {
      LEFT JOIN "like_status_enum" AS lse
      ON lse."id" = pcl."status"
      WHERE lse."status" = '${LikeStatusEnum.Like}'
+     AND pcl."commentId" = pc."id"
      ) AS "likesCount",
      (SELECT CAST(COUNT(*) AS INTEGER)
      FROM "posts_comments_likes" AS pcl
      LEFT JOIN "like_status_enum" AS lse
      ON lse."id" = pcl."status"
      WHERE lse."status" = '${LikeStatusEnum.Dislike}'
+     AND pcl."commentId" = pc."id"
      ) AS "dislikesCount",
      (SELECT lse."status"
      FROM "posts_comments_likes" AS pcl
      LEFT JOIN "like_status_enum" AS lse
      ON lse."id" = pcl."status"
      WHERE lse."status" != '${LikeStatusEnum.None}' AND
-     pcl."userId" = $1
+     pcl."userId" = $1 AND pcl."commentId" = pc."id"
      ) AS "myStatus"
     FROM "posts_comments" AS pc
     LEFT JOIN "users" AS u
@@ -123,7 +198,7 @@ export class CommentsQueryRepository {
     ON bb."blogId" = p."blogId"
     LEFT JOIN "blogs" AS b
     ON b."id" = p."blogId"
-    WHERE bb."isBanned" != true
+    WHERE bb."isBanned" != true OR bb."isBanned" IS NULL
     ),
     "temp_data2" as (
     SELECT * FROM "temp_data1" as td
@@ -204,9 +279,10 @@ export class CommentsQueryRepository {
       },
       createdAt: comment.createdAt,
       likesInfo: {
-        likesCount: comment.likesCount,
-        dislikesCount: comment.dislikesCount,
-        myStatus: comment.myStatus ?? LikeStatusEnum.None,
+        likesCount: comment.likesCount ?? comment.likesInfo.likesCount,
+        dislikesCount: comment.dislikesCount ?? comment.likesInfo.dislikesCount,
+        myStatus:
+          comment.myStatus ?? comment.likesInfo.myStatus ?? LikeStatusEnum.None,
       },
     };
   }
