9c1dc99d18d343a236bcd940cae7ba61ed4a1caa
alexanderbalaban24 <alexanderbalaban24@gmail.com>
2023-08-20 18:05:50 +0300

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -16,18 +16,18 @@ export class AppService {
 
   async deleteAllData(): Promise<boolean> {
     try {
-      await this.dataSource.query(`DELETE FROM "users_ban"`);
-      await this.dataSource.query(`DELETE FROM "users_email_confirmation"`);
-      await this.dataSource.query(`DELETE FROM "users_password_recovery"`);
-      await this.dataSource.query(`DELETE FROM "users_devices"`);
-      await this.dataSource.query(`DELETE FROM "blogs_ban"`);
-      await this.dataSource.query(`DELETE FROM "users_ban_for_blog"`);
-      await this.dataSource.query(`DELETE FROM "posts_comments_likes"`);
-      await this.dataSource.query(`DELETE FROM "posts_comments"`);
-      await this.dataSource.query(`DELETE FROM "posts_likes"`);
-      await this.dataSource.query(`DELETE FROM "posts"`);
-      await this.dataSource.query(`DELETE FROM "blogs"`);
-      await this.dataSource.query(`DELETE FROM "users"`);
+      //await this.dataSource.query(`DELETE FROM "users_ban"`);
+      //await this.dataSource.query(`DELETE FROM "users_email_confirmation"`);
+      //await this.dataSource.query(`DELETE FROM "users_password_recovery"`);
+      //await this.dataSource.query(`DELETE FROM "users_devices"`);
+      //await this.dataSource.query(`DELETE FROM "blogs_ban"`);
+      //await this.dataSource.query(`DELETE FROM "users_ban_for_blog"`);
+      //await this.dataSource.query(`DELETE FROM "posts_comments_likes"`);
+      //await this.dataSource.query(`DELETE FROM "posts_comments"`);
+      //await this.dataSource.query(`DELETE FROM "posts_likes"`);
+      //await this.dataSource.query(`DELETE FROM "posts"`);
+      //await this.dataSource.query(`DELETE FROM "blogs"`);
+      //await this.dataSource.query(`DELETE FROM "users"`);
 
       return true;
     } catch (e) {
