6672ce13351305bf3feaf2d9e421588a0abc8077
alexanderbalaban24@gmail.com <a.balaban@pixelplex.io>
2023-09-24 02:07:54 +0300

    fix like-status logic

--- src/features/posts/application/use-cases/like-status-post-use-case.ts
+++ src/features/posts/application/use-cases/like-status-post-use-case.ts
@@ -20,7 +20,18 @@ export class LikeStatusPostUseCase
   constructor(private likesRepository: PostsLikeRepository) {}
 
   async execute(command: LikeStatusPostCommand): Promise<ResultDTO<null>> {
-    const like = new PostLike();
+    const likeResult = await this.likesRepository.findLike(
+      +command.postId,
+      +command.userId,
+    );
+
+    let like: PostLike;
+    if (likeResult.payload) {
+      like = likeResult.payload;
+    } else {
+      like = new PostLike();
+    }
+
     like.postId = +command.postId;
     like.userId = +command.userId;
     like.status = command.likeStatus;
