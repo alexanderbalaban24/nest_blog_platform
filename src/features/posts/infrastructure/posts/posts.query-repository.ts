3bbf7ebce422c5aa2048c241b06a888af4e75245
alexanderbalaban24@gmail.com <a.balaban@pixelplex.io>
2023-09-24 01:47:50 +0300

    hotfix

--- src/features/posts/infrastructure/posts/posts.query-repository.ts
+++ src/features/posts/infrastructure/posts/posts.query-repository.ts
@@ -121,7 +121,7 @@ export class PostsQueryRepository {
     userId?: number,
   ): Promise<ResultDTO<ViewPostModel>> {
     console.log(
-      'DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD',
       postId,
       userId,
     );
